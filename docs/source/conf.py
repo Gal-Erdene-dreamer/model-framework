dcabe42ddc4ea4bcd453bb91576ef9533bd09905
N. Geertjens <56881763+Lycolus@users.noreply.github.com>
2022-03-11 19:39:54 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -45,7 +45,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', '_templates']
 
-autodoc_mock_imports = ['matplotlib', 'numpy', 'pandas']
+autodoc_mock_imports = ['matplotlib', 'numpy', 'pandas', 'scipy']
 
 
 
